065de9bf469d402ffdd42d01ccb03057b67d6721
James Hooper <jameshooperdesigns@gmail.com>
2022-01-26 17:47:56 -0700

    Add a tags to the navigation

---
+++ script.js
@@ -0,0 +1,11 @@
+const box1 = document.querySelector('.box1');
+const box2 = document.querySelector('.box2');
+
+console.log(box1);
+console.log(box2);
+
+box2.style.display = 'none';
+
+const circleNav = document.querySelectorAll('.nav-dot');
+console.log(circleNav);
+
